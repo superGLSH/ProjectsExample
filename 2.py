e73f8b759ff371f45fb1ebb1ea95719a46abbf4e
superGLSH <57950753+superGLSH@users.noreply.github.com>
2023-06-09 18:50:50 +0300

    Add files via upload
    Небольшой скриптик на CV2, который отмечает пиксели яркости выше 700 по 3 компонентам.

---
+++ 2.py
@@ -0,0 +1,28 @@
+import cv2
+
+
+def f(im, x, y):
+    for i in range(-20, 21):
+        for j in range(-20, 21):
+            try:
+                if im[x + i][y + j] != False:
+                    return False
+            except Exception:
+                pass
+    return True
+
+
+image = cv2.imread("roflan.jpeg")
+maska = [len(image[0]) * [False] for i in range(len(image))]
+for i in range(len(image)):
+    for j in range(len(image[0])):
+        yark = sum(image[i][j])
+        if yark > 700:
+            if f(maska, i, j):
+                maska[i][j] = True
+for i in range(len(maska)):
+    for j in range(len(maska[0])):
+        if maska[i][j]:
+            cv2.circle(image, (j, i), 20, [0, 0, 255])
+cv2.imshow('', image)
+cv2.waitKey(0)
